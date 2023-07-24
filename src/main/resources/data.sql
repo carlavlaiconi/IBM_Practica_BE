8ffce1ec723bf55f97fdb541781c87f169e34517
b-iulia <iulia.bonto03@e-uvt.ro>
2023-07-24 23:29:01 +0300

    updated2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-                                     id INT AUTO_INCREMENT PRIMARY KEY ,
+                                     id INT AUTO_INCREMENT PRIMARY KEY,
                                      databaseVersion LONG,
                                      createDate TIMESTAMP,
                                      updateDate TIMESTAMP,
@@ -8,9 +8,10 @@ CREATE TABLE IF NOT EXISTS users (
     role VARCHAR(100) NOT NULL
     );
 INSERT INTO users (name, email, role) VALUES ('John Doe', 'john.doe@example.com', 'leader');
-INSERT INTO users ( name, email, role) VALUES ('Jane Smith', 'jane.smith@example.com', 'mentor');
+INSERT INTO users (name, email, role) VALUES ('Jane Smith', 'jane.smith@example.com', 'mentor');
+
 CREATE TABLE IF NOT EXISTS team (
-                                    id INT AUTO_INCREMENT PRIMARY KEY ,
+                                    id INT AUTO_INCREMENT PRIMARY KEY,
                                     databaseVersion LONG,
                                     createDate TIMESTAMP,
                                     updateDate TIMESTAMP,
@@ -18,8 +19,9 @@ CREATE TABLE IF NOT EXISTS team (
     );
 INSERT INTO team (name) VALUES ('Team A');
 INSERT INTO team (name) VALUES ('Team B');
+
 CREATE TABLE IF NOT EXISTS activity (
-                                        id INT AUTO_INCREMENT PRIMARY KEY ,
+                                        id INT AUTO_INCREMENT PRIMARY KEY,
                                         databaseVersion LONG,
                                         createDate TIMESTAMP,
                                         updateDate TIMESTAMP,
@@ -27,8 +29,9 @@ CREATE TABLE IF NOT EXISTS activity (
     );
 INSERT INTO activity (name) VALUES ('Activity 1');
 INSERT INTO activity (name) VALUES ('Activity 2');
+
 CREATE TABLE IF NOT EXISTS session (
-                                       id INT AUTO_INCREMENT PRIMARY KEY ,
+                                       id INT AUTO_INCREMENT PRIMARY KEY,
                                        databaseVersion LONG,
                                        createDate TIMESTAMP,
                                        updateDate TIMESTAMP,
@@ -36,8 +39,9 @@ CREATE TABLE IF NOT EXISTS session (
     activity_id INT
     );
 INSERT INTO session (date, activity_id) VALUES ('2023-07-18', 1);
+
 CREATE TABLE IF NOT EXISTS attendance (
-                                          id INT AUTO_INCREMENT PRIMARY KEY ,
+                                          id INT AUTO_INCREMENT PRIMARY KEY,
                                           databaseVersion LONG,
                                           createDate TIMESTAMP,
                                           updateDate TIMESTAMP,
@@ -47,8 +51,9 @@ CREATE TABLE IF NOT EXISTS attendance (
 );
 INSERT INTO attendance (student_id, session_id, present) VALUES (2, 1, true);
 INSERT INTO attendance (student_id, session_id, present) VALUES (1, 1, false);
+
 CREATE TABLE IF NOT EXISTS grade (
-                                     id INT AUTO_INCREMENT PRIMARY KEY ,
+                                     id INT AUTO_INCREMENT PRIMARY KEY,
                                      database_version LONG,
                                      create_date TIMESTAMP,
                                      update_date TIMESTAMP,
