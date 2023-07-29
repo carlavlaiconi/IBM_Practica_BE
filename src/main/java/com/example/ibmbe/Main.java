f6465b25ca5554a0f8e6c0078a69f21ba1da73e3
alexandra <teoranalexandra0@gmail.com>
2023-07-29 15:22:57 +0300

    update

--- src/main/java/com/example/ibmbe/Main.java
+++ src/main/java/com/example/ibmbe/Main.java
@@ -2,8 +2,10 @@ package com.example.ibmbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.example.ibmbe")
+@EnableJpaRepositories(basePackages = "com.example.ibmbe")
 public class Main {
 	public static void main(String[] args) {
 
